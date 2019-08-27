bfdb0d5ceb4ddb04a6a39f58d6d9c5495e80a097
Lauro Cabral <lauro199471@gmail.com>
2019-08-27 00:10:33 -0700

    Added Delete

--- todo_list/app.js
+++ todo_list/app.js
@@ -10,11 +10,19 @@ const generateTemplate = todo => {
     `;
 	list.innerHTML += html;
 };
+
 addForm.addEventListener('submit', e => {
 	e.preventDefault();
 	const todo = addForm.add.value.trim();
 	if (todo.length) {
 		generateTemplate(todo);
 		addForm.reset();
+	}
+});
+
+// Delete todos
+list.addEventListener('click', e =>{
+	if(e.target.classList.contains('delete')){
+		e.target.parentElement.remove();
 	}
 });
